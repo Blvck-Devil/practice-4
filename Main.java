1a1b4e6ca7bc25c063268b2ffe8ab2f64897def1
Blvck-Devil <112816848+Blvck-Devil@users.noreply.github.com>
2022-09-06 08:11:51 +0000

    -

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+// input the length and the width of a rectangle and calculate and print the perimeter. 
+import java.util.*;
+public class Main {
+
+    public static void main(String args[]) {
+      int Length,Width;
+      int perimeter;
+
+      Scanner a=new Scanner(System.in);
+
+      System.out.println("Perimeter of a Rectangle");
+        System.out.println("Input Length:");
+      int length=a.nextInt();
+      
+        System.out.println("Input Width:");
+      int width=a.nextInt();
+
+      perimeter=2*(length+width);
+
+      System.out.println("Perimeter is:"+perimeter);
+      
+    }
 }
